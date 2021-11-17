a0d4025495effc3a2f3e20d9a319c408d85fe510
Felix Brüll <felixbruell@live.de>
2021-11-17 15:54:35 +0100

    added ssao pass and adjusted startup script

---
+++ startup.py
@@ -0,0 +1,107 @@
+# Graphs
+from falcor import *
+
+def render_graph_SSAO():
+    g = RenderGraph('SSAO')
+    loadRenderPassLibrary('MinimalPathTracer.dll')
+    loadRenderPassLibrary('AccumulatePass.dll')
+    loadRenderPassLibrary('BSDFViewer.dll')
+    loadRenderPassLibrary('CSM.dll')
+    loadRenderPassLibrary('Antialiasing.dll')
+    loadRenderPassLibrary('BlitPass.dll')
+    loadRenderPassLibrary('DebugPasses.dll')
+    loadRenderPassLibrary('DualDepthPass.dll')
+    loadRenderPassLibrary('DepthPass.dll')
+    loadRenderPassLibrary('ForwardLightingPass.dll')
+    loadRenderPassLibrary('ErrorMeasurePass.dll')
+    loadRenderPassLibrary('FLIPPass.dll')
+    loadRenderPassLibrary('GBuffer.dll')
+    loadRenderPassLibrary('HBAOPlus.dll')
+    loadRenderPassLibrary('SSAO.dll')
+    loadRenderPassLibrary('TemporalDelayPass.dll')
+    loadRenderPassLibrary('ImageLoader.dll')
+    loadRenderPassLibrary('MegakernelPathTracer.dll')
+    loadRenderPassLibrary('OptixDenoiser.dll')
+    loadRenderPassLibrary('PassLibraryTemplate.dll')
+    loadRenderPassLibrary('PixelInspectorPass.dll')
+    loadRenderPassLibrary('SceneDebugger.dll')
+    loadRenderPassLibrary('SimplePostFX.dll')
+    loadRenderPassLibrary('SkyBox.dll')
+    loadRenderPassLibrary('SVGFPass.dll')
+    loadRenderPassLibrary('TestPasses.dll')
+    loadRenderPassLibrary('ToneMapper.dll')
+    loadRenderPassLibrary('Utils.dll')
+    loadRenderPassLibrary('WhittedRayTracer.dll')
+    GBufferRaster = createPass('GBufferRaster', {'samplePattern': SamplePattern.Center, 'sampleCount': 16, 'useAlphaTest': True, 'adjustShadingNormals': True, 'forceCullMode': False, 'cull': CullMode.CullBack})
+    g.addPass(GBufferRaster, 'GBufferRaster')
+    SSAO = createPass('SSAO', {'enabled': True, 'halfResolution': False, 'kernelSize': 16, 'noiseSize': uint2(16,16), 'radius': 2.0, 'distribution': SampleDistribution.UniformHammersley, 'blurWidth': 5, 'blurSigma': 2.0})
+    g.addPass(SSAO, 'SSAO')
+    g.addEdge('GBufferRaster.depth', 'SSAO.depth')
+    g.addEdge('GBufferRaster.faceNormalW', 'SSAO.normals')
+    g.markOutput('SSAO.ambientMap')
+    return g
+m.addGraph(render_graph_SSAO())
+
+def render_graph_HBAOPlus():
+    g = RenderGraph('HBAOPlus')
+    loadRenderPassLibrary('MinimalPathTracer.dll')
+    loadRenderPassLibrary('AccumulatePass.dll')
+    loadRenderPassLibrary('BSDFViewer.dll')
+    loadRenderPassLibrary('CSM.dll')
+    loadRenderPassLibrary('Antialiasing.dll')
+    loadRenderPassLibrary('BlitPass.dll')
+    loadRenderPassLibrary('DebugPasses.dll')
+    loadRenderPassLibrary('DualDepthPass.dll')
+    loadRenderPassLibrary('DepthPass.dll')
+    loadRenderPassLibrary('ForwardLightingPass.dll')
+    loadRenderPassLibrary('ErrorMeasurePass.dll')
+    loadRenderPassLibrary('FLIPPass.dll')
+    loadRenderPassLibrary('GBuffer.dll')
+    loadRenderPassLibrary('HBAOPlus.dll')
+    loadRenderPassLibrary('SSAO.dll')
+    loadRenderPassLibrary('TemporalDelayPass.dll')
+    loadRenderPassLibrary('ImageLoader.dll')
+    loadRenderPassLibrary('MegakernelPathTracer.dll')
+    loadRenderPassLibrary('OptixDenoiser.dll')
+    loadRenderPassLibrary('PassLibraryTemplate.dll')
+    loadRenderPassLibrary('PixelInspectorPass.dll')
+    loadRenderPassLibrary('SceneDebugger.dll')
+    loadRenderPassLibrary('SimplePostFX.dll')
+    loadRenderPassLibrary('SkyBox.dll')
+    loadRenderPassLibrary('SVGFPass.dll')
+    loadRenderPassLibrary('TestPasses.dll')
+    loadRenderPassLibrary('ToneMapper.dll')
+    loadRenderPassLibrary('Utils.dll')
+    loadRenderPassLibrary('WhittedRayTracer.dll')
+    HBAOPlus = createPass('HBAOPlus')
+    g.addPass(HBAOPlus, 'HBAOPlus')
+    DualDepthPass = createPass('DualDepthPass')
+    g.addPass(DualDepthPass, 'DualDepthPass')
+    g.addEdge('DualDepthPass.depth', 'HBAOPlus.depth')
+    g.addEdge('DualDepthPass.depth2', 'HBAOPlus.depth2')
+    g.markOutput('HBAOPlus.ambientMap')
+    return g
+m.addGraph(render_graph_HBAOPlus())
+
+# Scene
+m.loadScene('D:/scenes/obj/sponza/sponza.pyscene')
+m.scene.renderSettings = SceneRenderSettings(useEnvLight=True, useAnalyticLights=True, useEmissiveLights=True, useVolumes=True)
+m.scene.camera.position = float3(-13.569309,6.322368,2.217087)
+m.scene.camera.target = float3(-14.036203,6.108661,3.075189)
+m.scene.camera.up = float3(0.000000,1.000000,0.000000)
+m.scene.cameraSpeed = 1.0
+
+# Window Configuration
+m.resizeSwapChain(1920, 1017)
+m.ui = True
+
+# Clock Settings
+m.clock.time = 0
+m.clock.framerate = 0
+# If framerate is not zero, you can use the frame property to set the start frame
+# m.clock.frame = 0
+
+# Frame Capture
+m.frameCapture.outputDir = '.'
+m.frameCapture.baseFilename = 'Mogwai'
+
