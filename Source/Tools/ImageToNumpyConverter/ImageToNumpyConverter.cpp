cd8244b796e3ecbcea6f12d47bd42595327f81cb
kopaka1822 <felixbruell@live.de>
2022-12-08 21:33:30 +0100

    worked on numpy converter

--- Source/Tools/ImageToNumpyConverter/ImageToNumpyConverter.cpp
+++ Source/Tools/ImageToNumpyConverter/ImageToNumpyConverter.cpp
@@ -7,14 +7,16 @@ static const bool SkipTrivialSamples = true;
 
 int main(int argc, char* argv[])
 {
-    if (argc < 3)
+    if (argc < 5)
     {
-        std::cout << "Usage: ImageToNumpyConverter.exe <raster image> <ray image>" << std::endl;
+        std::cout << "Usage: ImageToNumpyConverter.exe <raster image> <ray image> <instances> <instance_center>" << std::endl;
         return 1;
     }
 
     gli::texture2d_array texRaster(gli::load(argv[1]));
     gli::texture2d_array texRay(gli::load(argv[2]));
+    gli::texture2d_array texInstances(gli::load(argv[3]));
+    gli::texture2d_array texCurInstance(gli::load(argv[4]));
 
     assert(texRaster.format() == gli::FORMAT_R32_SFLOAT_PACK32);
     //assert(texRaster.layers() == 8);
@@ -29,18 +31,25 @@ int main(int argc, char* argv[])
     std::vector<float> rasterSamples;
     std::vector<float> raySamples;
     std::vector<int> sameAsRay; // 1 if raster is same as ray, 0 otherwise
+    std::vector<int> sameInstance; // 1 if raster sample instance is same as pixel center
+    std::vector<int> rasterInstanceDiffs; // 0 if both neighbors are same instance, 1 if only one is same instance, 2 if both are different instances
     //std::vector<int> numInvalid; // number of invalid samples
     rasterSamples.reserve(width * height * nSamples);
     raySamples.reserve(width * height * nSamples);
     sameAsRay.reserve(width * height);
+    sameInstance.reserve(width * height * nSamples);
+    rasterInstanceDiffs.reserve(width * height * nSamples);
 
     std::vector<float> raster;
     raster.resize(nSamples);
     std::vector<float> ray;
     ray.resize(nSamples);
+    std::vector<uint32_t> instances;
+    instances.resize(nSamples);
 
     // fetch function for tex raster (and tex ray)
     auto fetch = gli::detail::convert<gli::texture2d_array, float, gli::defaultp>::call(texRaster.format()).Fetch;
+    auto fetchInt = gli::detail::convert<gli::texture2d_array, float, gli::defaultp>::call(texInstances.format()).Fetch;
     int nTrivial = 0;
     int nEmpty = 0;
     int nSame = 0;
@@ -48,11 +57,14 @@ int main(int argc, char* argv[])
 
     for(int y = 0; y < height; ++y) for(int x = 0; x < width; ++x)
     {
+        const auto curInstance = (uint32_t)fetchInt(texCurInstance, gli::extent2d(x, y), 0, 0, 0).r;;
+
         // obtain sample data
         for(size_t i = 0; i < nSamples; ++i)
         {
             raster[i] = fetch(texRaster, gli::extent2d(x, y), i, 0, 0).r;
             ray[i] = fetch(texRay, gli::extent2d(x, y), i, 0, 0).r;
+            instances[i] = (uint32_t)fetchInt(texInstances, gli::extent2d(x, y), i, 0, 0).r;
         }
 
         // skip pure zero entries (background)
@@ -73,10 +85,25 @@ int main(int argc, char* argv[])
         rasterSamples.insert(rasterSamples.end(), raster.begin(), raster.end());
         raySamples.insert(raySamples.end(), ray.begin(), ray.end());
 
+        for (size_t i = 0; i < nSamples; ++i)
+        {
+            sameInstance.push_back(instances[i] == curInstance ? 1 : 0);
+            uint32_t instDiff = 0;
+            instDiff += instances[i] != instances[(i + nSamples - 1) % nSamples] ? 1 : 0;
+            instDiff += instances[i] != instances[(i + 1) % nSamples] ? 1 : 0;
+            rasterInstanceDiffs.push_back(instDiff);
+        }
+
         // determine if ray is equal to raster
         //auto same = std::equal(raster.begin(), raster.end(), ray.begin());
         // set same to true if all values are almost equal (within 0.01f)
-        auto same = std::equal(raster.begin(), raster.end(), ray.begin(), [](float f1, float f2) { return std::abs(f1 - f2) < 0.01f; });
+        auto same = std::equal(raster.begin(), raster.end(), ray.begin(), [](float sRaster, float sRay){
+            // distance is small
+            if (std::abs(sRaster - sRay) < 0.05f) return true;
+            // distance is large but both are outside of the interesting area
+            if (sRay > 2.0f) return true;
+            return false; // ray sample is in interesting area (<2.0)
+        });
         sameAsRay.push_back(same ? 1 : 0);
         nSame += same ? 1 : 0;
         nDifferent += same ? 0 : 1;
@@ -94,6 +121,9 @@ int main(int argc, char* argv[])
     const unsigned long shape[] = {  (unsigned long)sameAsRay.size(), (unsigned long)nSamples }; // shape = rows, columns
     npy::SaveArrayAsNumpy("raster.npy", false, 2, shape, rasterSamples);
     npy::SaveArrayAsNumpy("ray.npy", false, 2, shape, raySamples);
+
+    npy::SaveArrayAsNumpy("sameInstance.npy", false, 2, shape, sameInstance);
+    npy::SaveArrayAsNumpy("rasterInstanceDiffs.npy", false, 2, shape, rasterInstanceDiffs);
 
     npy::SaveArrayAsNumpy("same.npy", false, 1, shape, sameAsRay);
 
---
+++ Source/Tools/ImageToNumpyConverter/visualize_depth_instance.py
@@ -0,0 +1,165 @@
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LogisticRegression
+
+# set current directory as working directory
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+# load raster.npy, ray.npy and same.npy as numpy arrays
+raster = np.load('raster.npy')
+ray = np.load('ray.npy')
+same = np.load('same.npy')
+sameInstance = np.load('sameInstance.npy')
+rasterInstanceDiffs = np.load('rasterInstanceDiffs.npy')
+
+# print number of 0's in same.npy
+numberValidSamples = np.count_nonzero(same) # (raster == ray)
+numberInvalidSamples = len(same) - numberValidSamples # (raster != ray))
+print("ray==raster for ", numberValidSamples, " samples")
+print("ray!=raster for ", numberInvalidSamples, " samples")
+print("score for random guessing: ", max(numberInvalidSamples, numberValidSamples) / len(same))
+
+# transform into single samples
+raster = raster.reshape(-1, 1)
+ray = ray.reshape(-1, 1)
+sameInstance = sameInstance.reshape(-1, 1)
+rasterInstanceDiffs = rasterInstanceDiffs.reshape(-1, 1)
+
+print("number of instance diff occurences [0, 1, 2]: ", np.count_nonzero(rasterInstanceDiffs == 0), np.count_nonzero(rasterInstanceDiffs == 1), np.count_nonzero(rasterInstanceDiffs == 2))
+
+
+
+# shorten data
+# remove all datapoints where raster < 1.0
+ray = ray[raster > 1.0]
+sameInstance = sameInstance[raster > 1.0]
+rasterInstanceDiffs = rasterInstanceDiffs[raster > 1.0]
+raster = raster[raster > 1.0]
+
+#same = np.logical_or((np.abs(raster - ray) < 0.05), (ray > 2.0))
+same = (np.abs(raster - ray) < 0.05)
+
+def percentOf(frac):
+	return np.round(frac * 100)
+
+print("number of samples where raster > 1.0: ", len(raster))
+print("individual sample ray==raster for ", percentOf(np.count_nonzero(same)  / len(same)), "% of samples")
+
+def printSameInstanceStats():
+	sameAndSameInstance = np.logical_and(same, sameInstance)
+	sameAndDifferentInstance = np.logical_and(same, np.logical_not(sameInstance))
+	print("percentage of same instance samples that are ray==raster", percentOf(np.count_nonzero(sameAndSameInstance) / np.count_nonzero(sameInstance) ))
+	print("percentage of different instance samples that are ray==raster", percentOf(np.count_nonzero(sameAndDifferentInstance) / np.count_nonzero(np.logical_not(sameInstance)) ))
+
+	x = [] # raster height range
+	y1 = [] # percentage of same instance samples that are ray==raster
+	y2 = [] # percentage of different instance samples that are ray==raster
+	for height in np.arange(1.0, 12.0, 0.1):
+		x.append(height + 0.05)
+		heightFilter = np.logical_and(raster >= height, raster < (height + 0.1))
+		numSamplesSameInstance = max(np.count_nonzero(np.logical_and(heightFilter, sameInstance)), 1)
+		numSamplesDifferentInstance = max(np.count_nonzero(np.logical_and(heightFilter, np.logical_not(sameInstance))),1)
+		y1.append(np.count_nonzero(np.logical_and(heightFilter, sameAndSameInstance)) / numSamplesSameInstance * 100)
+		#y1.append(np.count_nonzero(np.logical_and(heightFilter, sameAndSameInstance)))
+		y2.append(np.count_nonzero(np.logical_and(heightFilter, sameAndDifferentInstance)) / numSamplesDifferentInstance * 100)
+		#y2.append(np.count_nonzero(np.logical_and(heightFilter, sameAndDifferentInstance)))
+
+	plt.plot(x, y1, 'r-', lw=2)
+	plt.plot(x, y2, 'b-', lw=2)
+	# name the x axis
+	plt.xlabel('raster height')
+	plt.ylabel('raster==ray %')
+	# show a legend on the plot
+
+	# set x ticks to 1.0
+	plt.xticks(np.arange(1.0, 12.0, 1.0))
+	# show gridlines in x for every tick
+	plt.grid(True, which='major', axis='x')
+	
+	# do logistic regression based on raster heights only
+	logRaster = LogisticRegression(random_state=42, n_jobs=-1, penalty='none')
+	logRaster.fit(raster.reshape(-1, 1), same)
+	# plot the decision function of logRaster
+	xx = np.linspace(1.0, 12.0, 1000)
+	# get the decision function of logRaster
+	decision = logRaster.predict_proba(xx.reshape(-1, 1))
+	decision = decision[:, 1] # only use second colum (same prediction)
+	# plot the decision function
+	plt.plot(xx, decision * 100, 'k-', lw=2)
+	
+	# print accuracy of logistic regression
+	print("logistic regression accuracy (raster): ", logRaster.score(raster.reshape(-1, 1), same))
+	
+	# do logistic regrassion based on raster heights and sameInstance
+	logRasterAndSameInstance = LogisticRegression(random_state=42, n_jobs=-1, penalty='none')
+	logRasterAndSameInstance.fit(np.hstack((raster.reshape(-1, 1), sameInstance.reshape(-1, 1))), same)
+	# plot for same instance
+	decision = logRasterAndSameInstance.predict_proba(np.hstack((xx.reshape(-1, 1), np.ones((len(xx), 1)))))
+	decision = decision[:, 1] # only use second colum (same prediction)
+	plt.plot(xx, decision * 100, 'g-', lw=2)
+	# plot for different instance
+	decision = logRasterAndSameInstance.predict_proba(np.hstack((xx.reshape(-1, 1), np.zeros((len(xx), 1)))))
+	decision = decision[:, 1] # only use second colum (same prediction)
+	plt.plot(xx, decision * 100, 'y-', lw=2)
+
+	# print accuracy of logistic regression
+	print("logistic regression accuracy (raster+instance): ", logRasterAndSameInstance.score(np.hstack((raster.reshape(-1, 1), sameInstance.reshape(-1, 1))), same))
+
+	plt.legend(['same instance', 'different instance', 'logit (raster)', 'logit (raster, sameInstance)', 'logit (raster, differentInstance)'], loc='upper right')
+	plt.show()
+
+	print("number of instance diff occurences [0, 1, 2]: ", np.count_nonzero(rasterInstanceDiffs == 0), np.count_nonzero(rasterInstanceDiffs == 1), np.count_nonzero(rasterInstanceDiffs == 2))
+
+printSameInstanceStats()
+
+#while True:
+# get random seed
+seed = np.random.randint(0, 1000000)
+# shuffle all arrays with the same seed
+np.random.seed(seed)
+np.random.shuffle(raster)
+np.random.seed(seed)
+np.random.shuffle(ray)
+np.random.seed(seed)
+np.random.shuffle(same)
+np.random.seed(seed)
+np.random.shuffle(sameInstance)
+np.random.seed(seed)
+np.random.shuffle(rasterInstanceDiffs)
+
+# plot raster as x-axis, sameInstance as y-axis and same as color
+numElements = 10000
+#plt.scatter(raster[0:numElements], same[0:numElements], c=sameInstance[0:numElements], cmap='bwr', alpha=0.1)
+
+def setupPlot(plt):
+	plt.xlabel('raster height')
+	plt.ylabel('ray height')
+
+	# restrict x-axis to [-2, 10]
+	plt.xlim(0.0, 12)
+	plt.ylim(0.0, 12)
+
+	# show colorbar
+	plt.colorbar()
+	# set plot height
+	plt.gcf().set_size_inches(12, 12)
+
+	# plot line y=x
+	plt.plot([0, 32], [0, 32], 'k-', lw=2)
+	# show grid line for y=1 with gray color
+	plt.axhline(y=1, color='gray', linestyle='--')
+	plt.axhline(y=0, color='gray', linestyle='-')
+
+plt.title('same instance (red) or not (blue)')
+plt.scatter(raster[0:numElements], ray[0:numElements], c=sameInstance[0:numElements], cmap='bwr', alpha=0.3)
+setupPlot(plt)
+#plt.show()
+
+
+# do a second plit with rasterInstanceDiffs as color
+#plt.title('same instance (red) or not (blue)')
+#plt.scatter(raster[0:numElements], ray[0:numElements], c=rasterInstanceDiffs[0:numElements], cmap='rainbow', alpha=0.3)
+#setupPlot(plt)
+#plt.show()
