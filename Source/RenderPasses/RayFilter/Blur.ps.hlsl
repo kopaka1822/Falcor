3e3e562a04fef2a943891f9b6bfe121be9b6bcfb
kopaka1822 <felixbruell@live.de>
2023-04-09 20:28:11 +0200

    started adding wave intrinsics to filter

--- Source/RenderPasses/RayFilter/Blur.ps.hlsl
+++ Source/RenderPasses/RayFilter/Blur.ps.hlsl
@@ -5,6 +5,18 @@ uint main(float2 texC : TEXCOORD, float4 svPos : SV_POSITION) : SV_TARGET
     uint2 pixel = uint2(svPos.xy);
     uint center = gInput[pixel];
 
-    if (countbits(center) < 2) center = 0;
+    uint rayCount = countbits(center);
+
+    //uint numWaves = WaveGetLaneCount();
+    uint numWaves = WaveActiveSum(1);
+
+    // count of all other threads (not including self)
+    uint otherRayCount = WaveActiveSum(rayCount) - rayCount;
+
+    float threshold = 0.1f;
+    
+    if (otherRayCount < uint(threshold * numWaves))
+        center = 0;
+    
     return center;
 }
