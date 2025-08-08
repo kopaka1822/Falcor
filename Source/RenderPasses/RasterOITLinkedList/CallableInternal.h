d3c4caf839f0ede088eb723c849e1e1eabc0837c
Kopaka <felixbruell@live.de>
2025-08-08 18:13:06 +0200

    added first version of insertion sort

--- Source/RenderPasses/RasterOITLinkedList/CallableInternal.h
+++ Source/RenderPasses/RasterOITLinkedList/CallableInternal.h
@@ -4,16 +4,32 @@ FragData f[MAX_FRAGMENT];
 // insert from linked list
 uint next = p.head;
 uint count = 0;
-[unroll] for (uint i = 0; i < MAX_FRAGMENT && next < maxElements; ++i)
+/* [unroll] for (uint i = 0; i < MAX_FRAGMENT && next < maxElements; ++i)
 {
     BufferData data = gBuffer[next];
     next = data.next;
     f[i].depth = data.depth;
     f[i].color = data.color;
     count++;
+}*/
+[unroll] for (uint i = 0; i < MAX_FRAGMENT; ++i)
+{
+    if (next < maxElements)
+    {
+        BufferData data = gBuffer[next];
+        next = data.next;
+        f[i].depth = data.depth;
+        f[i].color = data.color;
+        count++;
+    }
+    else
+    {
+        f[i].depth = 3.402823466e+38f;
+        f[i].color = 0;
+    }
 }
 
-#if MAX_FRAGMENT < 32
+#if MAX_FRAGMENT <= 0
 
 // sort registers (insertion sort)
 [unroll] for (uint i = 1; i < MAX_FRAGMENT && i < count; ++i)
@@ -26,17 +42,12 @@ uint count = 0;
         f[j - 1] = tmp;
     }
 }
-#else
+#elif MAX_FRAGMENT >= 32
 // sort registers (shell sort)
 const uint gaps[] = { 111, 41, 13, 4, 1 };
 const uint maxGaps = 5;
-#if MAX_FRAGMENT <= 111
-const uint startGap = 1;
-#else
-const uint startGap = 0;
-#endif
 
-[unroll] for (uint gapIdx = startGap; gapIdx < maxGaps; ++gapIdx)
+[unroll] for (uint gapIdx = 0; gapIdx < maxGaps; ++gapIdx)
 {
     const int gap = gaps[gapIdx];
     for (uint i = gap; i < MAX_FRAGMENT && i < count; ++i)
@@ -46,6 +57,33 @@ const uint startGap = 0;
             let tmp = f[j];
             f[j] = f[j - gap];
             f[j - gap] = tmp;
+        }
+    }
+}
+
+#else
+// bitonic sort
+
+[unroll]
+for (uint size = 2; size <= MAX_FRAGMENT; size <<= 1)
+{
+    [unroll]
+    for (uint stride = size >> 1; stride > 0; stride >>= 1)
+    {
+        [unroll]
+        for (uint i = 0; i < MAX_FRAGMENT; ++i)
+        {
+            uint j = i ^ stride;
+            if (j > i)
+            {
+                bool ascending = ((i & size) == 0);
+                if ((f[i].depth > f[j].depth) == ascending)
+                {
+                    let tmp = f[i];
+                    f[i] = f[j];
+                    f[j] = tmp;
+                }
+            }
         }
     }
 }
