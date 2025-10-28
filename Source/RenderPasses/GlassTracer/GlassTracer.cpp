c12bbe0b3ae659853c79f787ca287f96349d65c5
kopaka <felixbruell@live.de>
2025-10-28 16:37:14 +0100

    made template for glass tracer based on ditherv2

---
+++ Source/RenderPasses/GlassTracer/GlassTracer.cpp
@@ -0,0 +1,272 @@
+/***************************************************************************
+ # Copyright (c) 2015-23, NVIDIA CORPORATION. All rights reserved.
+ #
+ # Redistribution and use in source and binary forms, with or without
+ # modification, are permitted provided that the following conditions
+ # are met:
+ #  * Redistributions of source code must retain the above copyright
+ #    notice, this list of conditions and the following disclaimer.
+ #  * Redistributions in binary form must reproduce the above copyright
+ #    notice, this list of conditions and the following disclaimer in the
+ #    documentation and/or other materials provided with the distribution.
+ #  * Neither the name of NVIDIA CORPORATION nor the names of its
+ #    contributors may be used to endorse or promote products derived
+ #    from this software without specific prior written permission.
+ #
+ # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS "AS IS" AND ANY
+ # EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ # PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ # CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ # EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ # PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ # PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ # OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ **************************************************************************/
+#include "GlassTracer.h"
+#include "RenderGraph/RenderPassStandardFlags.h"
+
+namespace
+{
+    const std::string kVbuffer = "vbuffer";
+    const std::string kMotion = "mvec";
+    const std::string kColorOut = "color";
+    const std::string kDepthOut = "depth";
+
+    const uint32_t kMaxPayloadSizeBytes = 6 * sizeof(float);
+    const std::string kProgramRaytraceFile = "RenderPasses/GlassTracer/GlassTracer.rt.slang";
+
+    const std::string kUseWhitelist = "useWhitelist";
+    const std::string kWhitelist = "whitelist";
+    const std::string kWhitelistBuffer = "whitelistBuffer"; // GPU Buffer for whitelist
+
+    const std::string kDebug = "debug";
+}
+
+extern "C" FALCOR_API_EXPORT void registerPlugin(Falcor::PluginRegistry& registry)
+{
+    registry.registerClass<RenderPass, GlassTracer>();
+}
+
+GlassTracer::GlassTracer(ref<Device> pDevice, const Properties& props)
+    : RenderPass(pDevice)
+{
+    mpSampleGenerator = SampleGenerator::create(mpDevice, SAMPLE_GENERATOR_UNIFORM);
+    mpSamplePattern = HaltonSamplePattern::create(16);
+
+    // load properties
+    for (const auto& [key, value] : props)
+    {
+        if (key == kUseWhitelist) mUseTransparencyWhitelist = value;
+        else if (key == kWhitelist)
+        {
+            std::stringstream ss;
+            std::string svalue = value;
+            ss << svalue;
+            std::string entry;
+            while (std::getline(ss, entry, ','))
+            {
+                mTransparencyWhitelist.insert(entry);
+            }
+        }
+    }
+}
+
+Properties GlassTracer::getProperties() const
+{
+    Properties props;
+    props[kUseWhitelist] = mUseTransparencyWhitelist;
+    // convert whitelist into a comma separated string
+    std::stringstream ss;
+    for (const auto& entry : mTransparencyWhitelist) ss << entry << ",";
+    props[kWhitelist] = ss.str();
+    return props;
+}
+
+RenderPassReflection GlassTracer::reflect(const CompileData& compileData)
+{
+    uint2 dims = getRenderSize(compileData.defaultTexDims, mRenderScale);
+
+    // Define the required resources here
+    RenderPassReflection reflector;
+    reflector.addOutput(kVbuffer, "V-buffer").format(HitInfo::kDefaultFormat).texture2D(dims.x, dims.y);
+    reflector.addOutput(kMotion, "Motion vector").format(ResourceFormat::RG32Float).flags(RenderPassReflection::Field::Flags::Optional).texture2D(dims.x, dims.y);
+    reflector.addOutput(kColorOut, "Final color").format(ResourceFormat::RGBA32Float).bindFlags(ResourceBindFlags::AllColorViews).texture2D(dims.x, dims.y);
+    reflector.addOutput(kDepthOut, "Depth").format(ResourceFormat::R32Float).bindFlags(ResourceBindFlags::AllColorViews).texture2D(dims.x, dims.y);
+
+    reflector.addOutput(kDebug, "Debug output").format(ResourceFormat::RGBA32Float).bindFlags(ResourceBindFlags::AllColorViews).texture2D(dims.x, dims.y, 1, 1, mPathLength);
+    return reflector;
+}
+
+void GlassTracer::execute(RenderContext* pRenderContext, const RenderData& renderData)
+{
+    if (mOptionsChanged)
+    {
+        auto& dict = renderData.getDictionary();
+        auto refreshFlags = dict.getValue(kRenderPassRefreshFlags, RenderPassRefreshFlags::None);
+        refreshFlags |= RenderPassRefreshFlags::RenderOptionsChanged;
+        dict[kRenderPassRefreshFlags] = refreshFlags;
+        mOptionsChanged = false;
+    }
+
+    auto pVbuffer = renderData.getTexture(kVbuffer);
+    auto pMotion = renderData.getTexture(kMotion);
+    auto pColor = renderData.getTexture(kColorOut);
+    auto pDepth = renderData.getTexture(kDepthOut);
+    auto pDebug = renderData.getTexture(kDebug);
+
+    if (!mpScene)
+    {
+        pRenderContext->clearTexture(pColor.get(), float4(0, 0, 0, 0));
+        return;
+    }
+
+    assert(mpProgram);
+    assert(mpVars);
+
+    uint2 frameDim = uint2(pVbuffer->getWidth(), pVbuffer->getHeight());
+    mpScene->getCamera()->setPatternGenerator(mpSamplePattern, 1.0f / float2(frameDim));
+
+    auto var = mpVars->getRootVar();
+    var["gVBuffer"] = pVbuffer;
+    var["gMotion"] = pMotion;
+    var["gColor"] = pColor;
+    var["gDepth"] = pDepth;
+    assert(mpTransparencyWhitelist);
+    var["gTransparencyWhitelist"] = mpTransparencyWhitelist;
+
+    if (pDebug)
+    {
+        pRenderContext->clearTexture(pDebug.get(), float4(0, 0, 0, 0));
+        var["gDebugTex"] = pDebug;
+    }
+
+    var["PerFrame"]["gFrameCount"] = mFrameCount;
+    var["PerFrame"]["gPathLength"] = mPathLength;
+    var["PerFrame"]["gAmbientIntensity"] = mAmbientIntensity;
+    var["PerFrame"]["gAnalyticIntensity"] = mAnalyticIntensity;
+    var["PerFrame"]["gEmissionIntensity"] = mEmissionIntensity;
+    var["PerFrame"]["gEnvmapIntensity"] = mEnvmapIntensity;
+    var["PerFrame"]["gPointLightClip"] = mPointLightClip;
+    var["PerFrame"]["gShadowLodBias"] = mShadowLodBias;
+    var["PerFrame"]["gEnableShadows"] = mEnableShadows ? 1 : 0;
+    var["PerFrame"]["gRoughnessCutoff"] = mRoughnessCutoff;
+
+    mpProgram->addDefine("TRANSPARENCY_WHITELIST", mUseTransparencyWhitelist ? "1" : "0");
+    mpProgram->addDefine("CULL_BACK_FACES", mCullBackFaces ? "1" : "0");
+    mpProgram->addDefine("MVEC", std::to_string(uint32_t(mMotionVector)));
+
+    uint3 dispatch = uint3(1);
+    dispatch.x = pVbuffer->getWidth();
+    dispatch.y = pVbuffer->getHeight();
+    mpScene->raytrace(pRenderContext, mpProgram.get(), mpVars, dispatch);
+
+    // add whitelist to dict
+    if (mUseTransparencyWhitelist)
+    {
+        renderData.getDictionary()[kWhitelist] = mTransparencyWhitelist;
+        renderData.getDictionary()[kWhitelistBuffer] = mpTransparencyWhitelist;
+    }
+    mFrameCount++;
+}
+
+void GlassTracer::renderUI(Gui::Widgets& widget)
+{
+    if (widget.dropdown("Render Scale", mRenderScale))
+        requestRecompile();
+
+    bool c = false; // changed
+
+    c |= widget.var("Path Length", mPathLength, 1, 64);
+
+    c |= widget.var("Roughness Cutoff", mRoughnessCutoff, 0.0f, 1.0f);
+    widget.tooltip("Surfaces with lower roughness will not reflect");
+
+    c |= widget.dropdown("Motion Vectors", mMotionVector);
+
+    if (auto g = widget.group("Scene"))
+    {
+        c |= widget.checkbox("Cull Back Faces", mCullBackFaces);
+
+        c |= widget.checkbox("Transparency Whitelist", mUseTransparencyWhitelist);
+        widget.tooltip("Uses only whitelisted materials for dithering, when enabled. If not whitelisted, the material will use an alpha test.");
+        if (mUseTransparencyWhitelist && mpScene)
+        {
+            auto g2 = widget.group("Whitelist");
+            std::string removeEntry;
+            // list all material names of the current scene
+            for (uint mat = 0; mat < mpScene->getMaterialCount(); ++mat)
+            {
+                std::string name = mpScene->getMaterial(MaterialID(mat))->getName();
+                bool isTransparent = mTransparencyWhitelist.find(name) != mTransparencyWhitelist.end();
+                if (g2.checkbox(name.c_str(), isTransparent))
+                {
+                    if (isTransparent) mTransparencyWhitelist.insert(name);
+                    else mTransparencyWhitelist.erase(name);
+                    updateWhitelistBuffer();
+                }
+            }
+        }
+    }
+
+    if (auto g = widget.group("Lighting"))
+    {
+        c |= widget.var("Ambient", mAmbientIntensity, 0.0f);
+        c |= widget.var("Analytic", mAnalyticIntensity, 0.0f);
+        c |= widget.var("Emission", mEmissionIntensity, 0.0f);
+        c |= widget.var("Envmap", mEnvmapIntensity, 0.0f);
+    }
+    if (auto g = widget.group("Shadows"))
+    {
+        c |= widget.checkbox("Enable Shadows", mEnableShadows);
+        c |= widget.var("Point Light Clip", mPointLightClip, 0.0f);
+        c |= widget.var("Shadow LOD Bias", mShadowLodBias, -16.0f, 16.0f, 0.5f);
+    }
+
+    mOptionsChanged |= c;
+}
+
+void GlassTracer::setScene(RenderContext* pRenderContext, const ref<Scene>& pScene)
+{
+    mpScene = pScene;
+    setupProgram();
+    mUseTransparencyWhitelist = updateWhitelistBuffer();
+}
+
+void GlassTracer::setupProgram()
+{
+    if (!mpScene) return;
+
+    DefineList defines;
+    defines.add(mpScene->getSceneDefines());
+    defines.add(mpSampleGenerator->getDefines());
+
+    RtProgram::Desc desc;
+    desc.addShaderModules(mpScene->getShaderModules());
+    desc.addShaderLibrary(kProgramRaytraceFile);
+    desc.addTypeConformances(mpScene->getTypeConformances());
+    desc.setMaxPayloadSize(kMaxPayloadSizeBytes);
+    desc.setMaxAttributeSize(mpScene->getRaytracingMaxAttributeSize());
+    desc.setMaxTraceRecursionDepth(1);
+    desc.setShaderModel("6_6");
+
+    ref<RtBindingTable> sbt = RtBindingTable::create(1, 1, mpScene->getGeometryCount());
+    sbt->setRayGen(desc.addRayGen("rayGen"));
+    sbt->setMiss(0, desc.addMiss("miss"));
+    sbt->setHitGroup(0, mpScene->getGeometryIDs(Scene::GeometryType::TriangleMesh), desc.addHitGroup("closestHit", "anyHit"));
+
+    mpProgram = RtProgram::create(mpDevice, desc, defines);
+    mpVars = RtProgramVars::create(mpDevice, mpProgram, sbt);
+
+    // Bind static resources.
+    ShaderVar var = mpVars->getRootVar();
+    mpSampleGenerator->setShaderData(var);
+}
+
+bool GlassTracer::updateWhitelistBuffer()
+{
+    return updateWhitelist(mpDevice, mpScene, mTransparencyWhitelist, mpTransparencyWhitelist);
+}
+
---
+++ Source/RenderPasses/GlassTracer/GlassTracer.h
@@ -0,0 +1,147 @@
+/***************************************************************************
+ # Copyright (c) 2015-23, NVIDIA CORPORATION. All rights reserved.
+ #
+ # Redistribution and use in source and binary forms, with or without
+ # modification, are permitted provided that the following conditions
+ # are met:
+ #  * Redistributions of source code must retain the above copyright
+ #    notice, this list of conditions and the following disclaimer.
+ #  * Redistributions in binary form must reproduce the above copyright
+ #    notice, this list of conditions and the following disclaimer in the
+ #    documentation and/or other materials provided with the distribution.
+ #  * Neither the name of NVIDIA CORPORATION nor the names of its
+ #    contributors may be used to endorse or promote products derived
+ #    from this software without specific prior written permission.
+ #
+ # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS "AS IS" AND ANY
+ # EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ # PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ # CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ # EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ # PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ # PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ # OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ **************************************************************************/
+#pragma once
+#include "Falcor.h"
+#include "RenderGraph/RenderPass.h"
+#include "Utils/SampleGenerators/HaltonSamplePattern.h"
+#include "../DitherVBuffer/TransparencyWhitelist.h"
+
+using namespace Falcor;
+
+class GlassTracer : public RenderPass
+{
+public:
+    // based on DLSS scales
+    enum class RenderScale : uint32_t
+    {
+        Full,
+        Quality,
+        Balanced,
+        Performance,
+        UtraPerformance,
+    };
+
+    FALCOR_ENUM_INFO(RenderScale, {
+        {RenderScale::Full, "Full (100%)"},
+        {RenderScale::Quality, "Quality (66.7%)"},
+        {RenderScale::Balanced, "Balanced (58%)"},
+        {RenderScale::Performance, "Performance (50%)"},
+        {RenderScale::UtraPerformance, "UltraPerformance (33.3%)"},
+        });
+
+    enum class MotionVector : uint32_t
+    {
+        FirstHit,
+        HalfwayReflection,
+        RayDifferentials
+    };
+
+    FALCOR_ENUM_INFO(MotionVector, {
+        {MotionVector::FirstHit, "FirstHit"},
+        {MotionVector::HalfwayReflection, "HalfwayReflection"},
+        {MotionVector::RayDifferentials, "RayDifferentials"},
+        });
+
+    FALCOR_PLUGIN_CLASS(GlassTracer, "GlassTracer", "Path Tracer specialized for noise-free glass rendering");
+
+    static ref<GlassTracer> create(ref<Device> pDevice, const Properties& props) { return make_ref<GlassTracer>(pDevice, props); }
+
+    GlassTracer(ref<Device> pDevice, const Properties& props);
+
+    virtual Properties getProperties() const override;
+    virtual RenderPassReflection reflect(const CompileData& compileData) override;
+    virtual void compile(RenderContext* pRenderContext, const CompileData& compileData) override {}
+    virtual void execute(RenderContext* pRenderContext, const RenderData& renderData) override;
+    virtual void renderUI(Gui::Widgets& widget) override;
+    virtual void setScene(RenderContext* pRenderContext, const ref<Scene>& pScene) override;
+    virtual bool onMouseEvent(const MouseEvent& mouseEvent) override { return false; }
+    virtual bool onKeyEvent(const KeyboardEvent& keyEvent) override { return false; }
+
+    static uint2 getRenderSize(uint2 displaySize, RenderScale scale)
+    {
+        uint2 res = displaySize;
+        switch (scale)
+        {
+        case RenderScale::Quality:
+            res = uint2(ceil(float2(displaySize) * 0.667f));
+            break;
+        case RenderScale::Balanced:
+            res = uint2(ceil(float2(displaySize) * 0.58f));
+            break;
+        case RenderScale::Performance:
+            res = uint2(ceil(float2(displaySize) * 0.50f));
+            break;
+        case RenderScale::UtraPerformance:
+            res = uint2(ceil(float2(displaySize) * 0.333f));
+            break;
+        }
+        res = max(res, uint2(1));
+        return res;
+    }
+private:
+
+    void setupProgram();
+    // returns true if at least one material was whitelisted (or scene was invalid)
+    bool updateWhitelistBuffer();
+
+    ref<Scene> mpScene;
+
+    ref<RtProgram> mpProgram;
+    ref<RtProgramVars> mpVars;
+    ref<SampleGenerator> mpSampleGenerator;
+    ref<Buffer> mpTransparencyWhitelist;
+
+    uint mFrameCount = 0;
+
+    ref<CPUSampleGenerator> mpSamplePattern;
+    bool mUseTransparencyWhitelist = false;
+    whitelist_t mTransparencyWhitelist;
+
+    bool mCullBackFaces = false;
+    int mPathLength = 10;
+    MotionVector mMotionVector = MotionVector::HalfwayReflection;
+
+    RenderScale mRenderScale = RenderScale::Full;
+
+    // lighting settings
+    float mAmbientIntensity = 0.25f;
+    float mAnalyticIntensity = 0.5f;
+    float mEmissionIntensity = 1.0f;
+    float mEnvmapIntensity = 1.0f;
+    // shadow settings
+    bool mEnableShadows = true;
+    float mPointLightClip = 0.2f;
+    float mShadowLodBias = 0.0f;
+
+    float mRoughnessCutoff = 0.5f;
+
+    bool mOptionsChanged = true;
+};
+
+FALCOR_ENUM_REGISTER(GlassTracer::RenderScale);
+FALCOR_ENUM_REGISTER(GlassTracer::MotionVector);
